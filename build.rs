5f0f0e61fffc54d447f88a6a0e777f5167245a4e
AndreaGuarracino <andreaguarracino@outlook.com>
2025-06-10 18:18:30 -0500

    refactor: comment out bindgen dependency and related code in build script

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-extern crate bindgen;
+// extern crate bindgen;
 
 use std::{
     env,
@@ -39,11 +39,7 @@ fn build_wfa() -> Option<()> {
     
     let _ = remove_dir_all(build_paths.wfa_out());
 
-    // copy the WFA dir to OUT_PATH and build it there... clunky, but
-    // don't want to pull in the entire 100MB WFA repo, since git2
-    // doesn't seem to support shallow clones, and build scripts
-    // should only modify things inside OUT_PATH. since the WFA folder
-    // is just a couple MB, this is fine for now.
+    // Copy the WFA dir to OUT_PATH and build it there to avoid pulling the entire repo.
     let _cp_wfa = Command::new("cp")
         .arg("-r")
         .arg(build_paths.wfa_src())
@@ -51,14 +47,14 @@ fn build_wfa() -> Option<()> {
         .output()
         .expect("Copy failed");
 
-    // hotfix Makefile
-    let _makefile_fix = Command::new("sed")
-        .arg("-i")
-        .arg("s/CC_FLAGS=-Wall -g/CC_FLAGS=-Wall -g -fPIC/g")
-        .output()
-        .expect("Failed hotfixing makefile");
+    // Edit the Makefile
+    // let _makefile_fix = Command::new("sed")
+    //     .arg("-i")
+    //     .arg("s/CC_FLAGS=-Wall -g/CC_FLAGS=-Wall -g -fPIC/g")
+    //     .output()
+    //     .expect("Failed hotfixing makefile");
 
-
+    // Build the WFA library
     let output = Command::new("make")
         .arg("clean")
         .arg("all")
@@ -83,16 +79,7 @@ fn build_wfa() -> Option<()> {
 fn wfa() {
     let build_paths = BuildPaths::default();
 
-    // 1. Link instructions for Cargo.
-
-    // The directory of the WFA libraries, added to the search path.
-    // println!("cargo:rustc-link-search=../wfa2/lib");
-    // println!("cargo:rustc-link-search=WFA2-lib/lib");
-    // Link the `wfa-lib` library.
-    // Mind that the name of wfalib is set in the makefile, not here.
-    // Relevant part in makefile:
-    //  LIB_WFA=$(FOLDER_LIB)/libwfa.a
-    //  LIB_WFA_CPP=$(FOLDER_LIB)/libwfacpp.a
+    // Link instructions for Cargo
     // Despite the folder being named WFA2-lib, the library has to be linked with wfa
     println!("cargo:rustc-link-lib=wfa");
     // Also link `omp`.
@@ -101,30 +88,30 @@ fn wfa() {
     println!("cargo:rerun-if-changed={}/libwfa.a", build_paths.wfa_out_lib().display());
     // Rustc lib search for library in "OUT_DIR"
     println!("cargo:rustc-link-search={}", build_paths.wfa_out_lib().display());
-    // 2. Generate bindings.
 
-    let bindings = bindgen::Builder::default()
-        // Generate bindings for this header file.
-        // .header("../wfa2/wavefront/wavefront_align.h")
-        .header("WFA2-lib/wavefront/wavefront_align.h")
-        // Add this directory to the include path to find included header files.
-        // .clang_arg("-I../wfa2")
-        .clang_arg(format!("-I{}", build_paths.wfa_src().display()))
-        // Generate bindings for all functions starting with `wavefront_`.
-        .allowlist_function("wavefront_.*")
-        // Generate bindings for all variables starting with `wavefront_`.
-        .allowlist_var("wavefront_.*")
-        // Invalidate the built crate whenever any of the included header files
-        // changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-    // Write the bindings to the $OUT_DIR/bindings_wfa.rs file.
-    bindings
-        .write_to_file(build_paths.out_dir().join("bindings_wfa.rs"))
-        .expect("Couldn't write bindings!");
+    // Generate bindings
+    // let bindings = bindgen::Builder::default()
+    //     // Generate bindings for this header file.
+    //     // .header("../wfa2/wavefront/wavefront_align.h")
+    //     .header("WFA2-lib/wavefront/wavefront_align.h")
+    //     // Add this directory to the include path to find included header files.
+    //     // .clang_arg("-I../wfa2")
+    //     .clang_arg(format!("-I{}", build_paths.wfa_src().display()))
+    //     // Generate bindings for all functions starting with `wavefront_`.
+    //     .allowlist_function("wavefront_.*")
+    //     // Generate bindings for all variables starting with `wavefront_`.
+    //     .allowlist_var("wavefront_.*")
+    //     // Invalidate the built crate whenever any of the included header files
+    //     // changed.
+    //     .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+    //     // Finish the builder and generate the bindings.
+    //     .generate()
+    //     // Unwrap the Result and panic on failure.
+    //     .expect("Unable to generate bindings");
+    // // Write the bindings to the $OUT_DIR/bindings_wfa.rs file.
+    // bindings
+    //     .write_to_file(build_paths.out_dir().join("bindings_wfa.rs"))
+    //     .expect("Couldn't write bindings!");
 }
 
 fn main() {

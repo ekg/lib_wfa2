819b82cd842aabedcb962fd5b82380fe454234f6
Erik Garrison <erik.garrison@gmail.com>
2025-07-12 14:53:49 +0200

    clean up warnings

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,4 +1,10 @@
 #[allow(clippy::all)]
+#[allow(warnings)]
 pub mod wfa {
+    #![allow(warnings)]
+    #![allow(clippy::all)]
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
     include!("bindings_wfa.rs");
 }
